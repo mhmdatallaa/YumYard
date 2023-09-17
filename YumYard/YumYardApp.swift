975b24169beaf0e7f8d3768518e19292a4042ab5
Mohamed Atallah <mohamed.atallah440@gmail.com>
2023-09-17 15:19:02 +0300

    Initial commit

---
+++ YumYard/YumYardApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumYardApp.swift
+//  YumYard
+//
+//  Created by Mohamed Atallah on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct YumYardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
